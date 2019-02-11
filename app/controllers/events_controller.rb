9410f26d74ec71ca9bf8be25852e7951f42688b0
ichi.you <ichiyou311@gmail.com>
2019-02-11 15:11:50 +0000

    fix delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,7 +37,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     flash[:success] = 'イベントを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
 
   private
